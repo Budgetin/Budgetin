a33ba1c577250377509cbc9c435c5add40487451
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-01-18 08:49:54 +0700

    [BE-Edit] add rest_framwork in INSTALLED_APPS

--- budgetin/budgetin/settings.py
+++ budgetin/budgetin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'django_softdelete',
     'api.apps.ApiConfig',
 ]
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'budgetin',
         'USER': 'postgres',
-        'PASSWORD': 'harve',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
