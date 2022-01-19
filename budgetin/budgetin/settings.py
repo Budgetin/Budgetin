8b77745248b34b949c55106d3f67077e70880074
Kevin Draven Kenanga <kevindravenk@gmail.com>
2022-01-19 17:00:12 +0700

    [BE-Edit] My Bad

--- budgetin/budgetin/settings.py
+++ budgetin/budgetin/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_softdelete'
+    'django_softdelete',
     'api.apps.ApiConfig'
 ]
 
