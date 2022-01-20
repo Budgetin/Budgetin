f9a3b2c787a924c5a7a8391d0d671fd6b3e7fffe
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-01-20 14:44:07 +0700

    [BE-Edit] remove is_active from Coa

--- budgetin/api/models/coa_model.py
+++ budgetin/api/models/coa_model.py
@@ -7,6 +7,5 @@ class Coa(SoftDeleteModel, TimestampModel):
     name = models.CharField(max_length=200, unique=True)
     definition = models.CharField(max_length=1000, blank=True, null=True)
     hyperion_name = models.CharField(max_length=200)
-    is_active = models.BooleanField(default=True)
     is_capex = models.BooleanField(default=False)
     minimum_item_origin = models.IntegerField(blank=True, null=True)
