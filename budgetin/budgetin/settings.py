1683580318ae814812e7544b99217ff93e8de1a7
harvelouis <harvelouis18@gmail.com>
2022-01-20 17:19:39 +0700

    [BE - Edit] Add exception handler in settings

--- budgetin/budgetin/settings.py
+++ budgetin/budgetin/settings.py
@@ -53,6 +53,10 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig'
 ]
 
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'api.exceptions.custom_exception_handler.custom_exception_handler'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
