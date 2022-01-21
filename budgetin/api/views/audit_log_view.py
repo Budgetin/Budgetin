d2e2c94f0d0003955bdcb7234f611d1b08bff5d9
harvelouis <harvelouis18@gmail.com>
2022-01-21 16:21:35 +0700

    [BE - Edit] Edit Audit Log

--- budgetin/api/views/audit_log_view.py
+++ budgetin/api/views/audit_log_view.py
@@ -4,15 +4,15 @@ from api.serializers.audit_log_serializer import AuditLogSerializer
 
 
 class AuditLogViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
-    queryset = AuditLog.objects.none()
+    queryset = AuditLog.objects.all()
     serializer_class = AuditLogSerializer
-    def get_queryset(self):
-        if self.request.method == 'GET':
-            tableid = self.request.GET.get('table_id', None)
-            entityid = self.request.GET.get('entity_id', None)
-            if tableid is not None and entityid is not None:
-                queryset = AuditLog.objects.filter(entity_id=entityid).filter(table_id=tableid)
-            else:
-                queryset = None
-        return queryset
+    # def get_queryset(self):
+    #     if self.request.method == 'GET':
+    #         tableid = self.request.GET.get('table_id', None)
+    #         entityid = self.request.GET.get('entity_id', None)
+    #         if tableid is not None and entityid is not None:
+    #             queryset = AuditLog.objects.filter(entity_id=entityid).filter(table_id=tableid)
+    #         else:
+    #             queryset = None
+    #     return queryset
     
