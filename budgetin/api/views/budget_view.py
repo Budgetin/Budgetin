d425f9f35b60844cc260afadf38f4a8aa4967c8a
harvelouis <harvelouis18@gmail.com>
2022-01-21 15:30:15 +0700

    [BE - Add] Audit Log Global Function

--- budgetin/api/views/budget_view.py
+++ budgetin/api/views/budget_view.py
@@ -1,3 +1,4 @@
+from venv import create
 from rest_framework import viewsets
 from api.models.budget_model import Budget
 from api.serializers.budget_serializer import BudgetSerializer
@@ -5,3 +6,4 @@ from api.serializers.budget_serializer import BudgetSerializer
 class BudgetViewSet(viewsets.ModelViewSet):
     queryset = Budget.objects.all()
     serializer_class = BudgetSerializer
+    
--- budgetin/api/views/coa_view.py
+++ budgetin/api/views/coa_view.py
@@ -1,10 +1,11 @@
 import datetime
 from rest_framework import viewsets
 from api.models.coa_model import Coa
-from api.models.audit_log_model import AuditLog
 from api.serializers.coa_serializer import CoaSerializer
 from api.permissions import IsAuthenticated, IsAdmin
 
+#For Audit Logging
+from api.utils.auditlog import AuditLog
 
 class CoaViewSet(viewsets.ModelViewSet):
     queryset = Coa.objects.all()
@@ -13,23 +14,22 @@ class CoaViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         coa = super().create(request, *args, **kwargs)
-        serial_data = {
-            "name": coa.data['name'],
-            "definition": coa.data['definition'],
-            "hyperion_name": coa.data['hyperion_name'],
-        }
-        AuditLog.objects.create(timestamp=datetime.datetime.now(
-        ), modified_by=request.custom_user['id'], entity_id=coa.data['id'], serialized_data=serial_data, action_id_id=1, table_id_id=4)
+        AuditLog.Save(coa, request, AuditLog.Create, AuditLog.Coa)
+        # AuditLog.objects.create(timestamp=datetime.datetime.now(
+        # ), modified_by=request.custom_user['id'], entity_id=coa.data['id'], serialized_data=coa.data, action_id_id=1, table_id_id=4)
         return coa
 
     def update(self, request, *args, **kwargs):
-        coa_update = super().update(request, *args, **kwargs)
-        AuditLog.objects.create(timestamp=datetime.datetime.now(
-        ), modified_by=request.custom_user['id'], entity_id=kwargs['pk'], serialized_data=request.data, action_id_id=3, table_id_id=4)
-        return coa_update
+        coa = super().update(request, *args, **kwargs)
+        AuditLog.Save(coa, request, AuditLog.Update, AuditLog.Coa)
+        # AuditLog.objects.create(timestamp=datetime.datetime.now(
+        # ), modified_by=request.custom_user['id'], entity_id=kwargs['pk'], serialized_data=coa_update.data, action_id_id=3, table_id_id=4)
+        return coa
 
     def destroy(self, request, *args, **kwargs):
-        coa_destroy = super().destroy(request, *args, **kwargs)
-        AuditLog.objects.create(timestamp=datetime.datetime.now(), modified_by=request.custom_user['id'], entity_id=kwargs['pk'], serialized_data={
-                                "data": "destroyed"}, action_id_id=4, table_id_id=4)
-        return coa_destroy
+        coa = super().destroy(request, *args, **kwargs)
+        AuditLog.Save(coa, request, AuditLog.Delete, AuditLog.Coa)
+        #AuditLog.Save(coa, request, AuditLog.Delete, AuditLog.Coa)
+        # AuditLog.objects.create(timestamp=datetime.datetime.now(), modified_by=request.custom_user['id'], entity_id=kwargs['pk'], serialized_data={
+        #                         "data": "destroyed"}, action_id_id=4, table_id_id=4)
+        return coa
