192d1eaab96ac8e269749e07c64fb3e82c1b1e2d
Kevin Draven Kenanga <kevindravenk@gmail.com>
2022-01-24 10:24:26 +0700

    [BE-Edit] Update User

--- budgetin/api/views/user_view.py
+++ budgetin/api/views/user_view.py
@@ -1,8 +1,22 @@
 from rest_framework import viewsets
 from api.models.user_model import User
 from api.serializers.user_serializer import UserSerializer
-
+from api.utils.date_format import timestamp_to_strdateformat
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def list(self, request, *args, **kwargs):
+        user = super().list(request, *args, **kwargs)
+        for each in user.data:
+            #Reformat date
+            each['created_at'] = timestamp_to_strdateformat(each['created_at'], "%d %B %Y")
+            each['updated_at'] = timestamp_to_strdateformat(each['updated_at'], "%d %B %Y")
+        return user
+    
+    def retrieve(self, request, *args, **kwargs):
+        user = super().retrieve(request, *args, **kwargs)
+        user.data['created_at'] = timestamp_to_strdateformat(user.data['created_at'], "%d %B %Y")
+        user.data['updated_at'] = timestamp_to_strdateformat(user.data['updated_at'], "%d %B %Y")
+        return user
