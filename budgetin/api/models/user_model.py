71fd220c9f549c796f1abec15e590d1b10787645
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-01-24 13:28:07 +0700

    [BE-Edit] Refactor user model and migration

--- budgetin/api/models/user_model.py
+++ budgetin/api/models/user_model.py
@@ -1,26 +1,12 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 from api.models.abstract_model import TimestampModel, UserTrackModel
 from django_softdelete.models import SoftDeleteModel
 
-from api.managers import UserManager
 
-
-class User(AbstractBaseUser, PermissionsMixin, TimestampModel, UserTrackModel, SoftDeleteModel):
+class User(TimestampModel, UserTrackModel, SoftDeleteModel):
+    employee_id = models.BigIntegerField(unique=True)
+    display_name = models.CharField(max_length=100)
     username = models.CharField(max_length=100, unique=True)
     role = models.CharField(max_length=100, default='user')
     is_active = models.BooleanField(default=True)
-    password = None
-    is_superuser = None
-    date_joined = None
-    last_login = None
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
-
-    objects = UserManager()
-
-    def __str__(self):
-        return self.username
