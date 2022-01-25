6068754ec1dc2d17eb9cd7e8931361f0941b3181
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-01-25 17:45:54 +0700

    [BE-Add] Add IsManager permissions

---
+++ budgetin/api/permissions/is_manager.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+from api.exceptions import NotEligibleException
+
+
+class IsManager(permissions.BasePermission):
+    def has_permission(self, request, view):
+        eselon =  request.custom_user['eselon']
+        if eselon == 'S1' or eselon == 'S2' or eselon == 'S3':
+            return True
+        raise NotEligibleException()
