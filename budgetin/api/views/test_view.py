da84b95df0e2eadaeef357e830590a22b5d4f8f9
harvelouis <harvelouis18@gmail.com>
2022-01-26 10:34:52 +0700

    [BE - Add] Import Excel & Read

--- budgetin/api/views/test_view.py
+++ budgetin/api/views/test_view.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.utils.biro import create_update_all_biro
+
 
 class TestView(APIView):
     def get(self, request):
-        create_update_all_biro()
+        #print something
+        
         return Response({})
