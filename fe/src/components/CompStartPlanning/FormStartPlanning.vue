909b171ccb70470131eb1b6b9f7f5e934519b4c9
AngeliaClaresta <49297602+AngeliaClaresta@users.noreply.github.com>
2022-01-29 20:46:16 +0700

    [FE-Edit] Fix EditPlanning,Add TimelineLogPlanning

--- fe/src/components/CompStartPlanning/FormStartPlanning.vue
+++ fe/src/components/CompStartPlanning/FormStartPlanning.vue
@@ -24,7 +24,7 @@
                   outlined
                   return-object
                   :disabled="isView"
-                  :rules="validation.required.concat(validation.yearRule)">
+                  :rules="validation.required">
                 </v-text-field>
               </div>
             </v-col>
@@ -46,7 +46,7 @@
                   outlined
                   return-object
                   :disabled="isView"
-                  :rules="validation.required">
+                  :rules="validation.statusRule">
                 </v-select>
               </div>
             </v-col>
@@ -103,7 +103,7 @@
                   outlined
                   return-object
                   :disabled="isView"
-                  :rules="validation.required.concat(isNew)">
+                  :rules="validation.notifRule">
                 </v-select>
               </div>
             </v-col>
@@ -115,17 +115,18 @@
           <v-row no-gutters v-if="form.notification.id==1">
             <v-col> Send to <strong class="red--text">*</strong>
               <v-col no-gutters>
+                <div class="sendTo">
                 <v-select
-                  class="StartPlanning__select"
                   :items="dataAllBiro"
                   v-model="form.biros"
                   item-text="code"
-                  item-value="id"
+                  item-value="id, code"
                   placeholder="Choose Biro"
                   multiple
                   chips
                   outlined>
                 </v-select>
+                </div>
               </v-col>            
             </v-col>
           </v-row>
@@ -213,7 +214,7 @@
 import { mapState } from "vuex";
 export default {
   name: "FormStartPlanning",
-  props: ["form", "isNew", "isView", "dataAllBiro"],
+  props: ["form", "isNew", "isView"],
 
   data: () => ({
     //yearSubstring: new Date().getFullYear(),
@@ -226,18 +227,23 @@ export default {
       targetRule: [
         v => /^[0-9.,]+$/.test(v) || "This field is numbers only",
       ],
-      yearRule: v  => {
+      yearRule: [
         //if (!v.trim()) return true;
-        if (!isNaN(parseFloat(v)) && v >= 1000 && v <= 9999) return true;
-        return 'Year has to be integer and contains 4 digits';
-      },
+        v => { if (!isNaN(parseFloat(v)) && v >= 1000 && v <= 9999) return true;
+        return 'Year has to be integer and contains 4 digits'; }
+      ],
+      // yearRule: v  => {
+      //   //if (!v.trim()) return true;
+      //   if (!isNaN(parseFloat(v)) && v >= 1000 && v <= 9999) return true;
+      //   return 'Year has to be integer and contains 4 digits';
+      // },
     },
   }),
   
   computed: {
     ...mapState("statusInfo", ["statusInfoPlanning"]),
     ...mapState("statusInfo", ["statusNotification"]),
-    ...mapState("allBiro", ["getAllBiro"]),
+    ...mapState("allBiro", ["getAllBiro", "dataAllBiro"]),
 
     cardTitle() {
       return this.isNew ? "Add" : this.isView ? "View" : "Edit";
@@ -249,9 +255,9 @@ export default {
 
   methods: {
     onSubmit() {
-      console.log("Masuk Sini");
+      // console.log("Masuk Sini");
       let validate = this.$refs.form.validate();
-      console.log("Masuk Let Validate");
+      // console.log("Masuk Let Validate");
       if (validate) {
         const payload = {
           id: this.form.id,
