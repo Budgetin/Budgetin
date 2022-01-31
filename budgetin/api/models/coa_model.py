ca48e0b28f6721c082f30b6021870ffae3ba20dd
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-01-31 09:57:44 +0700

    [BE-Edit] Refactor COA

--- budgetin/api/models/coa_model.py
+++ budgetin/api/models/coa_model.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from django.db import models
 from django_softdelete.models import SoftDeleteModel
 
 from api.models.abstract_model import TimestampModel, UserTrackModel
-
+from api.models.user_model import User
 
 class Coa(SoftDeleteModel, TimestampModel, UserTrackModel):
     name = models.CharField(max_length=200)
@@ -10,3 +11,14 @@ class Coa(SoftDeleteModel, TimestampModel, UserTrackModel):
     hyperion_name = models.CharField(max_length=200)
     is_capex = models.BooleanField(default=False)
     minimum_item_origin = models.BigIntegerField(blank=True, null=True)
+
+    def format_timestamp(self):
+        self.created_at = self.created_at.strftime("%d %B %Y")
+        self.updated_at = self.updated_at.strftime("%d %B %Y")
+        
+    def format_created_updated_by(self):
+        self.created_by = User.objects.get(pk=self.created_by).display_name
+        if self.updated_by:
+            self.updated_by = User.objects.get(pk=self.updated_by).display_name
+        else:
+            self.updated_by = ''
--- budgetin/api/serializers/coa_serializer.py
+++ budgetin/api/serializers/coa_serializer.py
@@ -4,6 +4,8 @@ from api.models import Coa
 
 
 class CoaSerializer(serializers.ModelSerializer):
+    created_by = serializers.CharField()
+    updated_by = serializers.CharField()
     class Meta:
         model = Coa
         fields = '__all__'
