4ae4623b519261bec99c07962d4f0bd148be51ca
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-01-31 11:36:18 +0700

    [BE-Edit] Refactor strategy

--- budgetin/api/serializers/strategy_serializer.py
+++ budgetin/api/serializers/strategy_serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from api.models import Strategy
 class StrategySerializer(serializers.ModelSerializer):
+    created_by = serializers.CharField()
+    updated_by = serializers.CharField()
     class Meta:
         model = Strategy
         fields = '__all__'
