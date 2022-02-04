ce1313a83e9690cc1d0465794771b1513ce146b8
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-02-04 09:15:35 +0700

    [BE-Edit] Add Import excel (not done)

--- budgetin/api/urls.py
+++ budgetin/api/urls.py
@@ -7,9 +7,10 @@ app_name = 'api'
 urlpatterns = [
     path('biro/', views.BiroView.as_view()),
     path('login/', views.LoginView.as_view()),
+    path('login/user/', views.LoginUserView.as_view()),
     path('logout/', views.LogoutView.as_view()),
     path('test/', views.TestView.as_view()),
-    path('upload/', views.ImportExcelView.as_view()),
+    path('import/list_planning/', views.ImportListPlanning.as_view()),
     path('download_list_planning/', views.ExportListPlanning.as_view()),
     path('download_list_project/', views.ExportListProject.as_view()),
 ]
