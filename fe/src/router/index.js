ad622f4155e690489814c8ecd1a00caec77d4a96
harvelouis <harvelouis18@gmail.com>
2022-02-14 14:24:25 +0700

    [FE - Edit] Login ga kedip2

--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -6,6 +6,44 @@ import store from "@/store";
 
 Vue.use(VueRouter);
 
+function checkSession(next, url) {
+  try {
+    if (store.getters["login/isAuthenticated"]) {
+      next();
+      return;
+    } else {
+      store
+        .dispatch("login/setInitial")
+        .then(() => {
+          if (store.getters["login/isAuthenticated"] && url!=='/login') {
+            next();
+            return;
+          }
+          else if(store.getters["login/isAuthenticated"] && url=='/login'){
+            router.push({ name: 'StartPlanning'});
+            return;
+          }else{
+            router.push({ name: 'Login', params: { redirectUrl: url !== '' ? url : '/startPlanning/' } });
+            return;
+          }
+         }).catch((err) => {
+           if(err.response.status == 401 && url!=='/login'){
+            router.push({ name: 'Login', params: { redirectUrl: url !== '' ? url : '/startPlanning/' } });
+            return;
+           }else{
+            next();
+            return;
+           }
+          });;
+      return;
+    }
+  } catch (e) {
+    ("masuk e");
+    next();
+    return;
+  }
+}
+
 const routes = [
   {
     path: "/",
@@ -16,139 +54,201 @@ const routes = [
       {
         path: "coa",
         name: "Coa",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/COA/ListCoa"),
       },
       {
         path: "/coa/:id/edit",
         name: "EditMasterCoa",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/COA/EditMasterCoa"),
       },
       {
         path: "product",
         name: "MasterProduct",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/MasterProduct/MasterProduct"),
       },
       {
         path: "/product/:id/edit",
         name: "EditMasterProduct",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/MasterProduct/EditMasterProduct"),
       },
       {
         path: "strategy",
         name: "MasterStrategy",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/MasterStrategy/MasterStrategy"),
       },
       {
         path: "/strategy/:id/edit",
         name: "EditMasterStrategy",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/MasterStrategy/EditMasterStrategy"),
       },
       {
         path: "/user/:id/edit",
         name: "EditMasterUser",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/MasterUser/EditMasterUser"),
       },
       {
         path: "user",
         name: "MasterUser",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/MasterUser/MasterUser"),
       },
       {
         path: "startPlanning",
         name: "StartPlanning",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/StartPlanning/StartPlanning"),
       },
       {
         path: "/startPlanning/:id/monitor",
         name: "MonitorPlanning",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/StartPlanning/MonitorPlanning"),
       },
       {
         path: "/startPlanning/:id/view",
         name: "ViewPlanning",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/StartPlanning/ViewPlanning"),
       },
       {
         path: "/startPlanning/:id/viewStatusMonitor",
         name: "ViewStatusMonitoring",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/StartPlanning/ViewStatusMonitoring"),
       },
       {
         path: "projectList",
         name: "ListProject",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListProject/ListProject"),
       },
       {
         path: "/projectList/:id/view",
         name: "ViewListProject",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListProject/ViewListProject"),
       },
       {
         path: "/projectList/:id/viewProjectDetail",
         name: "ViewListProjectDetail",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListProject/ViewListProjectDetail"),
       },
       {
         path: "/projectList/:id/viewBudgetPlanning",
         name: "ViewListBudgetPlanning",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListProject/ViewListBudgetPlanning"),
       },
       {
         path: "/projectList/:id/viewBudgetRealization",
         name: "ViewListBudgetRealization",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListProject/ViewListBudgetRealization"),
       },
       {
-        path: "budgetList",
-        name: "budgetList",
+        path: "/budgetList",
+        name: "ListBudget",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListBudget/ListBudget"),
       },
       {
         path: "/budgetList/new",
         name: "ListBudgetNew",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListBudget/ListBudgetNew"),
       },
       {
         path: "/budgetList/existing",
         name: "ListBudgetExisting",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListBudget/ListBudgetExisting"),
       },
       {
         path: "/budgetList/:id/view",
         name: "EditListPlanning",
+        beforeEnter: (to, from, next) => {
+          checkSession(next, to.fullPath);
+        },
         component: () => import("@/views/ListBudget/EditListPlanning"),
       },
       {
         path: "/login",
         name: "Login",
         beforeEnter: (to, from, next) => {
-          try {
-            if (store.getters["login/isAuthenticated"]) {
-              store
-                .dispatch("login/setInitial")
-                .then(() => {
-                  if (!store.getters["login/isAuthenticated"]) {
-                    next();
-                    return;
-                  } else {
-                    router.go(-1);
-                    return;
-                  }
-                })
-                .catch((err) => {
-                  next();
-                  return;
-                });
-            } else {
-              next();
-              return;
-            }
-          } catch (e) {
-            ("masuk e");
-            next();
-            return;
-          }
+          checkSession(next, to.fullPath);
         },
+        // beforeEnter: (to, from, next) => {
+        //   try {
+        //     store
+        //         .dispatch("login/setInitial")
+        //         .then(() => {
+        //           if (!store.getters["login/isAuthenticated"]) {
+        //             next();
+        //             return;
+        //           } else {
+        //             var url = to.params.redirectUrl.toString();
+        //             router.push(url);
+        //             return;
+        //           }
+        //         })
+        //         .catch((err) => {
+        //           next();
+        //           return;
+        //         });
+        //   } catch (e) {
+        //     ("masuk e");
+        //     next();
+        //     return;
+        //   }
+        // },
         component: () => import("@/views/Login"),
       },
     ],
