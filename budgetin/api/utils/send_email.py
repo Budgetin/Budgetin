3304e7e6d0e3084763fbd363a5cb64bfbafc6016
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-02-15 13:47:58 +0700

    [BE-Edit] Set dummy email to fina

--- budgetin/api/utils/send_email.py
+++ budgetin/api/utils/send_email.py
@@ -2,12 +2,13 @@ from distutils.command.config import config
 from django.core.mail import send_mail
 
 def send_email(subject, body, receiver_email_list):
-    # send_mail(subject, body, from_email = 'noreply.budgetin@dti.co.id', receiver_email_list)
     send_mail(
         subject = subject, 
         message = body, 
         from_email = config('EMAIL_HOST_USER'), 
-        recipient_list=receiver_email_list)
+        # recipient_list=receiver_email_list) #DEBT, Uncommnet this line. delete line below
+        recipient_list='yosefina_santoso@dti.co.id'
+    )
     print('Sending email to: {}'.format(receiver_email_list))
     
     return True
