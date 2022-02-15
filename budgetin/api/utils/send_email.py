b3ab3ec34a4db02276b418c0b8d65db313907b28
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-02-15 14:05:04 +0700

    [BE-Edit] Fix send email

--- budgetin/api/utils/send_email.py
+++ budgetin/api/utils/send_email.py
@@ -1,11 +1,11 @@
-from distutils.command.config import config
+from django.conf import settings
 from django.core.mail import send_mail
 
 def send_email(subject, body, receiver_email_list):
     send_mail(
         subject = subject, 
         message = body, 
-        from_email = config('EMAIL_HOST_USER'), 
+        from_email = settings.EMAIL_HOST_USER, 
         # recipient_list=receiver_email_list) #DEBT, Uncommnet this line. delete line below
         recipient_list=['yosefina_santoso@dti.co.id']
     )
