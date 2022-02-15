e750cbbefae50c226ff491392d03ddea3ba829ec
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-02-15 14:22:43 +0700

    [BE-Edit] Add mytask retrieve endpoint

--- budgetin/api/views/task_view.py
+++ budgetin/api/views/task_view.py
@@ -2,9 +2,9 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
-from api.models import Monitoring
+from api.models import Monitoring, Budget
 from api.permissions import IsAuthenticated, IsUser
-from api.serializers import MonitoringSerializer
+from api.serializers import MonitoringSerializer, BudgetResponseSerializer
 
 class TaskViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated, IsUser]
@@ -17,6 +17,19 @@ class TaskViewSet(viewsets.ViewSet):
             monitoring.format_timestamp("%d %B %Y")                
 
         serializer = MonitoringSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        monitoring = Monitoring.objects.select_related('planning').get(pk=pk)
+        planning_id = monitoring.planning.id
+        print(planning_id)
+        queryset = Budget.objects.select_related('coa', 'project_detail', 'project_detail__planning', 
+                                                'project_detail__project', 'project_detail__project_type', 
+                                                'project_detail__project__biro', 'project_detail__project__product', 
+                                                'project_detail__project__product__strategy', 'created_by', 'updated_by')
+        queryset = queryset.filter(project_detail__planning__id=planning_id)
+        
+        serializer = BudgetResponseSerializer(queryset, many=True)
         return Response(serializer.data)
         
         
