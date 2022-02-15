8c9cbde36d93783bba713a332ba8932f2b9980f7
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-02-15 15:27:23 +0700

    [BE-Edit] Filter mytask based on user biro

--- budgetin/api/views/mytask_view.py
+++ budgetin/api/views/mytask_view.py
@@ -22,12 +22,15 @@ class TaskViewSet(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         monitoring = Monitoring.objects.select_related('planning').get(pk=pk)
         planning_id = monitoring.planning.id
-        print(planning_id)
+
+        user_ithc_biro = request.custom_user['ithc_biro']        
+        
         queryset = Budget.objects.select_related('coa', 'project_detail', 'project_detail__planning', 
                                                 'project_detail__project', 'project_detail__project_type', 
                                                 'project_detail__project__biro', 'project_detail__project__product', 
                                                 'project_detail__project__product__strategy', 'created_by', 'updated_by')
         queryset = queryset.filter(project_detail__planning__id=planning_id)
+        queryset = queryset.filter(project_detail__project__biro__ithc_biro=user_ithc_biro)
         
         serializer = BudgetResponseSerializer(queryset, many=True)
         return Response(serializer.data)
