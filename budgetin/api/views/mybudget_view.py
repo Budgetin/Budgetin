5cfd16f138831fcf4e1b8abe34a0f6054545a21e
Kevin Draven Kenanga <kevindravenk@gmail.com>
2022-02-16 08:56:11 +0700

    [BE-Edit] Fix MyTask and MyBudget

--- budgetin/api/views/mybudget_view.py
+++ budgetin/api/views/mybudget_view.py
@@ -9,14 +9,13 @@ class MyBudgetViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated, IsUser]
      
     def list(self, request):
-        print(request.custom_user)
         user_ithc_biro = request.custom_user['ithc_biro']
 
         queryset = Budget.objects.select_related('coa', 'project_detail', 'project_detail__planning', 
                                                 'project_detail__project', 'project_detail__project_type', 
                                                 'project_detail__project__biro', 'project_detail__project__product', 
                                                 'project_detail__project__product__strategy', 'updated_by', 'created_by').all()
-        queryset = queryset.filter(project_detail__project__biro=user_ithc_biro)
+        queryset = queryset.filter(project_detail__project__biro__ithc_biro=user_ithc_biro)
         
         for budget in queryset:
             budget.format_timestamp("%d %B %Y")
