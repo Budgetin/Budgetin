896cc7bf1a9473bca68ecfd58bc95337a1c35d6e
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-02-18 17:40:52 +0700

    [BE-Add] add import product endpoint

--- budgetin/api/views/product_view.py
+++ budgetin/api/views/product_view.py
@@ -1,14 +1,16 @@
 from django.utils.translation import ugettext_lazy as _
+from django.db import transaction
 from rest_framework import viewsets
 from rest_framework.response import Response
-from django.db import transaction
+from rest_framework.decorators import action
 
 from api.permissions import IsAuthenticated, IsAdminOrReadOnly
-from api.models import Product
+from api.models import Product, Strategy
 from api.serializers import ProductSerializer, ProductResponseSerializer
 from api.utils.auditlog import AuditLog
 from api.utils.enum import ActionEnum, TableEnum
-from api.exceptions.validation_exception import ValidationException
+from api.utils.file import read_excel
+from api.exceptions import ValidationException, ImportValidationException
 
 def is_product_duplicate(product_id, product_code, product_name):
     if Product.objects.filter(product_code=product_code).exclude(id=product_id) or Product.objects.filter(product_name=product_name).exclude(id=product_id):
@@ -62,3 +64,43 @@ class ProductViewSet(viewsets.ModelViewSet):
         product = super().destroy(request, *args, **kwargs)
         AuditLog.Save(product, request, ActionEnum.DELETE, TableEnum.PRODUCT)
         return product
+    
+    @transaction.atomic
+    @action(methods=['post'], detail=False, url_path='import')
+    def import_from_excel(self, request):
+        file = request.FILES['file'].read()
+        df = read_excel(file, 'product')
+        
+        for index, row in df.iterrows():
+            self.insert_to_db(request, row, (index+2))
+        
+        return Response(status=204)    
+
+    def insert_to_db(self, request, data, index):
+        code = data['product_code']
+        if self.product_already_exists(code):
+            raise ImportValidationException("Product '{}' at line {} already exists".format(code, index))
+        
+        strategy_name = data['strategy_name']
+        strategy = self.get_strategy_or_raise_error(strategy_name, index)
+            
+        product = self.create_product(request, data, strategy)
+        AuditLog.Save(ProductSerializer(product), request, ActionEnum.CREATE, TableEnum.PRODUCT) 
+            
+    def product_already_exists(self, code):
+        return Product.objects.filter(product_code=code).count() > 0
+    
+    def get_strategy_or_raise_error(self, name, index):
+        strategy = Strategy.objects.filter(name=name)
+        if strategy.count() == 0:
+            raise ImportValidationException("Strategy '{}' at line {} does not exists".format(name, index))
+        return strategy.first()
+    
+    def create_product(self, request, data, strategy):
+        return Product.objects.create(
+            product_code = data['product_code'],
+            product_name = data['product_name'],
+            strategy = strategy,
+            created_by_id = request.custom_user['id'],
+            updated_by_id = request.custom_user['id'],
+        )
