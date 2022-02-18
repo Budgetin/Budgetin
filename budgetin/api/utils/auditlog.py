6b5eb48fe9819b2b483be58ee4ea2d15d9448b8a
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-02-18 10:15:02 +0700

    [BE-Edit] Fix AuditLog

--- budgetin/api/utils/auditlog.py
+++ budgetin/api/utils/auditlog.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 import json
 
 from django.forms import model_to_dict
-from api.models import AuditLog
+from api.models import AuditLog as AL
 from api.utils.enum import ActionEnum
 class AuditLog():    
     def Save(data, request, action_enum, table_enum):
@@ -14,7 +14,7 @@ class AuditLog():
             entity_id = request.parser_context['kwargs']['pk']
             serialized_data = json.dumps({"message":"entity with id {} deleted from table {}".format(entity_id, table_enum.value)})
             
-        AuditLog.objects.create(
+        AL.objects.create(
             timestamp=timezone.now(), 
             modified_by=request.custom_user['id'], 
             entity_id=entity_id, 
