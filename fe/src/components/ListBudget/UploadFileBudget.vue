1ca096a91a5b46bdf5484893225ccc321f5e9800
Fina Santoso <finaokta3110@gmail.com>
2022-02-18 14:00:26 +0700

    [FE-Add] Validation and Loading when import budget and realization

--- fe/src/components/ListBudget/UploadFileBudget.vue
+++ fe/src/components/ListBudget/UploadFileBudget.vue
@@ -51,7 +51,10 @@ export default {
     name:"",
     files: [],
     validation: {
-      uploadRule: [(v) => !!v || "File is required"],
+      uploadRule: [
+        v => !!v || "File is required",
+        v => (v && v.size > 0) || 'File is required',
+      ],
     },
   }),
 
@@ -66,6 +69,9 @@ export default {
     },
     onSubmitUpload() {
       let validate = this.$refs.form.validate();
+      console.log(validate)
+      console.log(this.files)
+
       if (validate) {
         let data = {
           files: this.files,
