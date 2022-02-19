d842bc86c187fc423b357c1897371f25c4e47f9f
BCADOMAIN\U071182 <dionisiusst2@gmail.com>
2022-02-19 15:33:18 +0700

    [BE-Edit] set strategy to be nullable in product model

--- budgetin/api/models/product_model.py
+++ budgetin/api/models/product_model.py
@@ -7,7 +7,7 @@ from api.models.abstract_model import TimestampModel, UserTrackModel
 class Product(SoftDeleteModel, TimestampModel, UserTrackModel):
     product_code = models.CharField(max_length=200)
     product_name = models.CharField(max_length=200)
-    strategy = models.ForeignKey('Strategy',on_delete=models.CASCADE)
+    strategy = models.ForeignKey('Strategy',on_delete=models.CASCADE, null=True)
     is_active = models.BooleanField(default=True)
 
     all_object = models.Manager()
