bf68a66004dc9951a6a2284508205a102af44466
Fina Santoso <finaokta3110@gmail.com>
2022-02-23 14:43:20 +0700

    [FE-Dashboard] Add Chart

--- fe/src/views/Dashboard/Dashboard.vue
+++ fe/src/views/Dashboard/Dashboard.vue
@@ -1,9 +1,35 @@
 <template>
   <v-container>
     <v-card class="pt-5">
+      <div class="row sparkboxes my-4">
+        <div class="col-md-3">
+          <div class="box box1 ml-5">
+            <h1>2022</h1>
+            <h2>Year</h2>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="box box2">
+            <h1>1.213.123.123</h1>
+            <h2>Budget Planning</h2>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="box box3">
+            <h1>1.213.123.123</h1>
+            <h2>Budget Realisasi</h2>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="box box4 mr-5">
+            <h1>1.213.123.123</h1>
+            <h2>Forecast</h2>
+          </div>
+        </div>
+      </div>
       <!-- <iframe src="https://sense-demo.qlik.com/single/?appid=b23be62b-79d1-4761-b576-00ebc19acfb3&sheet=GZGbMWW&opt=ctxmenu,currsel" style="border:none;width:100%;height:1000px;"></iframe> -->
-        <!-- <iframe src="http://kp2misdb03/Reports/powerbi/Dashboard/GSIT/ITHC/Test?rs:embed=true" style="border:none;width:100%;height:1000px;"></iframe> -->
-        <apexchart  type="area" height="350" :options="chartOptions" :series="series"></apexchart>
+      <!-- <iframe src="http://kp2misdb03/Reports/powerbi/Dashboard/GSIT/ITHC/DashboarDomisili?rs:embed=true" style="border:none;width:100%;height:1000px;"></iframe> -->
+      <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
       <!-- </div> -->
     </v-card>
   </v-container>
@@ -12,10 +38,8 @@
 <script>
 export default {
   name: "Dashboard",
-  components: {
+  components: {},
 
-  },
-  
   created() {
     this.setBreadcrumbs();
   },
@@ -29,33 +53,63 @@ export default {
       ]);
     },
   },
-  data: () => ({          
-    series: [{
-      name: 'series1',
-      data: [31, 40, 28, 51, 42, 109, 100]
-    }, {
-      name: 'series2',
-      data: [11, 32, 45, 32, 34, 52, 41]
-    }],
+  data: () => ({
+    series: [
+      {
+        name: "Planning",
+        data: [44,  57,  61,  63,  66],
+      },
+      {
+        name: "Realization",
+        data: [76,  101,  87,  91,  94],
+      },
+    ],
     chartOptions: {
       chart: {
-        width: '100%',
+        type: "bar",
         height: 350,
-        type: 'area'
       },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "55%",
+          endingShape: "rounded",
+        },
+      },
+      colors:['#31e3e6', '#fea197'],
+      // colors:['#6adaf0', '#ff89a8'],
+
+      
       dataLabels: {
-        enabled: false
+        enabled: false,
       },
       stroke: {
-        curve: 'smooth'
+        show: true,
+        width: 2,
+        colors: ["transparent"],
       },
       xaxis: {
-        type: 'datetime',
-        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        categories: [
+          "APT",
+          "IIO",
+          "DMO",
+          "GAQ",
+          "DIS",
+        ],
+      },
+      yaxis: {
+        title: {
+          text: "",
+        },
+      },
+      fill: {
+        opacity: 1,
       },
       tooltip: {
-        x: {
-          format: 'dd/MM/yy HH:mm'
+        y: {
+          formatter: function (val) {
+            return "$ " + val + " thousands";
+          },
         },
       },
     },
@@ -63,62 +117,57 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-#master-coa {
-  width: 80%;
-  margin: 0px auto;
+<style scoped>
+::v-deep h2,
+h2,
+h4 {
+  display: flex;
+  justify-content: center;
+  font-weight: 600;
+  color: white;
+}
+</style>
 
-  .master-coa__header {
-    padding-left: 32px;
-    font-size: 1.25rem;
-    font-weight: 600;
-  }
-
-  .master-coa__input {
-    padding: 10px 32px;
-  }
-
-  .master-coa__btn {
-    text-align: end;
-
-    button {
-      margin: 10px 32px;
-    }
-  }
-
-  .master-coa__container {
-    padding: 24px 0px;
-    // box-shadow: rgb(0 0 0 / 35%) 0px 5px 15px;
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-    border-radius: 8px;
-  }
-
-  .master-coa__card {
-    button {
-      width: 8rem;
-    }
-  }
+<style>
+h1,
+h2,
+h2,
+h4 {
+  display: flex;
+  justify-content: center;
+  font-weight: 600;
+  color: white;
+}
+.box {
+  background-color: #2b2d3e;
+  padding: 25px 20px;
 }
 
-@media only screen and (max-width: 600px) {
-  /* For mobile phones */
-  #master-coa {
-    .master-coa__btn {
-      text-align: center;
-      padding: 0px 32px;
+.sparkboxes .box {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  text-shadow: 0 1px 1px 1px #666;
+  box-shadow: 0px 1px 15px 1px rgba(69, 65, 78, 0.08);
+  border-radius: 5px;
+}
 
-      button {
-        width: 100%;
-        margin: 0px 0px 32px 0px;
-      }
-    }
-    .master-coa__card {
-      flex-direction: column;
-      button {
-        width: 16rem !important;
-        margin-left: unset !important;
-      }
-    }
-  }
+.sparkboxes .box1 {
+  /* background-color: #0396ff; */
+  background-image: linear-gradient(135deg, #abdcff 10%, #0396ff 100%);
+}
+
+.sparkboxes .box2 {
+   /* background-color: #1B4332; */
+  background-image: linear-gradient(135deg, #2afadf 10%, #4c83ff 100%);
+}
+
+.sparkboxes .box3 {
+   /* background-color: #fd6585; */
+  background-image: linear-gradient(135deg, #ffd3a5 10%, #fd6585 100%);
+}
+
+.sparkboxes .box4 {
+   /* background-color: #744EC2; */
+  background-image: linear-gradient(135deg, #ee9ae5 10%, #5961f9 100%);
 }
 </style>
